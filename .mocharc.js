01c1874c27c6006daae5ce3a6c6c725af2c4f811
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2022-11-18 22:31:16 +0200

    fixed all code hopefully

--- .mocharc.js
+++ .mocharc.js
@@ -1,13 +1,13 @@
 // .mocharc.js
 
 module.exports = {
-    recursive: true,
-    reporter: 'spec',
-    timeout: 5000,
-    require: [
-        'chai/register-assert',  // Using Assert style
-        'chai/register-expect',  // Using Expect style
-        'chai/register-should',  // Using Should style
-        'ts-node/register',
-    ]
+  recursive: true,
+  reporter: 'spec',
+  timeout: 5000,
+  require: [
+    'chai/register-assert.js', // Using Assert style
+    'chai/register-expect.js', // Using Expect style
+    'chai/register-should.js', // Using Should style
+    'ts-node/register',
+  ],
 };
